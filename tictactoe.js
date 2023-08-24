9eae8da06f1f1e3a8a512160044a319e1e78e12d
Joenne Amancio <91231086+jm-amancio@users.noreply.github.com>
2023-08-24 09:55:40 +0800

    Add function checking for winner

--- tictactoe.js
+++ tictactoe.js
@@ -19,6 +19,9 @@ const Gameboard = (() =>  {
     const board = [];       // the gameboard array
     const gameboardUI = document.querySelector('.gameboard');
 
+    const getRow = () => row;
+    const getColumn = () => column;
+
     // fills the gameboard array with 9 cells
     function createGameboard (){
         for(let i=0; i<row; i++){
@@ -72,14 +75,14 @@ const Gameboard = (() =>  {
         }
     }
 
-    return {createGameboard, createGameboardUI, getGameboard, setGameboard, printBoard};
+    return {getRow, getColumn, createGameboard, createGameboardUI, getGameboard, setGameboard, printBoard};
 })();
 
 const DisplayController = (() => {
     // creates players and marks
     const players = [];
-    players.push(Player('OnePM', 'O'));
-    players.push(Player('TwoPM', 'X'));
+    players.push(Player('1', 'O'));
+    players.push(Player('2', 'X'));
 
     // getActivePlayer && switches player turn
     let getActivePlayer = players[0];
@@ -88,32 +91,74 @@ const DisplayController = (() => {
         ? (getActivePlayer = players[1])
         : (getActivePlayer = players[0]);
 
-    function newGame(){
+    function initialGame(){
         Gameboard.createGameboard();
         Gameboard.createGameboardUI();
+        status.textContent = `Player ${getActivePlayer.getName()}'s Turn`;
     }
 
     function restartGame(){
         Gameboard.createGameboard();
         Gameboard.printBoard();
+        getActivePlayer = players[0];
+        status.textContent = `Player ${getActivePlayer.getName()}'s Turn`;
     }
 
+    const status = document.querySelector('.status');
     function printNewRound(rowIndex, colIndex){
         let isMoveValid = Gameboard.setGameboard(rowIndex, colIndex, getActivePlayer.getMark());
         if(isMoveValid){
             Gameboard.printBoard();
             // check winning logic
+            if (isWinner(getActivePlayer.getMark())) {
+                status.textContent = `Player ${getActivePlayer.getName()} Wins`;
+
+                return;
+            }
             switchPlayerTurn();
+            status.textContent = `Player ${getActivePlayer.getName()}'s Turn`;
         }
     }
 
-    function isWinner(){
+    function isWinner(playerMark){
+        let board = Gameboard.getGameboard();
+        let rowAlignment = 3, columnAlignment = 3;
         
+        // check diagonal alignment
+        if (((board[0][0].getMark() == playerMark && board[2][2].getMark() == playerMark) ||  
+            (board[0][2].getMark() == playerMark && board[2][0].getMark() == playerMark)) && 
+            (board[1][1].getMark() == playerMark)){
+            return true;
+        }
+
+        for (let i=0; i<Gameboard.getRow(); i++){
+            rowAlignment = 3;
+            columnAlignment = 3;
+            for(let j=0; j<Gameboard.getColumn();j++){
+                // check row alignment
+                if (board[i][j].getMark() === playerMark) {
+                    rowAlignment--;
+                    if(rowAlignment===0){
+                        return true;
+                    }
+                }
+
+                // check column alignment
+                if (board[j][i].getMark() === playerMark) {
+                    columnAlignment--;
+                    if(columnAlignment===0){
+                        return true;
+                    }
+                }
+            }
+        }
+        
+        return false;
     }
 
-    return {switchPlayerTurn, printNewRound, newGame, restartGame};
+    return {switchPlayerTurn, printNewRound, initialGame, restartGame};
 })();
 
-DisplayController.newGame();
+DisplayController.initialGame();
 const restartButton = document.getElementById('restart');
 restartButton.addEventListener('click',() => DisplayController.restartGame());
